6c5d6699def8b7ee0cd952b2eba8340f95c8e6ec
Marcel Martins <triplom@gmail.com>
2024-05-05 14:28:11 +0100

    Parte 2 Atualizada - 05.05

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,69 @@
-
 import java.io.File
+import kotlin.reflect.KClass
 import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.full.findAnnotation
 
+// Anotação para indicar uma classe que implementa a transformação a ser aplicada à string por padrão
+@Retention(AnnotationRetention.RUNTIME)
+annotation class XmlString(val value: KClass<out XmlStringAdapter<*>>)
+
+// Anotação para associar um adaptador que realiza alterações na entidade XML após o mapeamento automático
+@Retention(AnnotationRetention.RUNTIME)
+annotation class XmlAdapter(val value: KClass<out XmlAdapterBase>)
+
+// Interface para adaptadores XML personalizados
+interface XmlAdapterBase {
+    fun adapt(element: XMLElement)
+}
+
+// Interface para adaptadores de string XML personalizados
+interface XmlStringAdapter<T> {
+    fun adapt(value: String): String
+}
+
+// Implementação padrão de um adaptador XML que não faz nada
+class NoOpXmlAdapter : XmlAdapterBase {
+    override fun adapt(element: XMLElement) {
+        // Não faz nada
+    }
+}
+
+// Implementação padrão de um adaptador de string XML que retorna o valor original
+class NoOpXmlStringAdapter<T> : XmlStringAdapter<T> {
+    override fun adapt(value: String): String {
+        return value.toString()
+    }
+}
+
+// Implementação de um adaptador de string XML que adiciona "%" ao valor
+class AddPercentageXmlStringAdapter : XmlStringAdapter<Int> {
+    override fun adapt(value: String): String {
+        return "$value%"
+    }
+}
+
+// Implementação do adaptador de personalização pós-mapeamento para a classe FUC
+class FUCAdapter : XmlAdapterBase {
+    override fun adapt(element: XMLElement) {
+        // Implemente as alterações necessárias na entidade XML após o mapeamento automático para a classe FUC
+    }
+}
+
 // Anotação para personalizar a tradução para XML
 @Target(AnnotationTarget.PROPERTY)
-annotation class XMLProperty(val name: String, val type: XMLType = XMLType.ATTRIBUTE, val ignore: Boolean = false)
+annotation class XMLProperty(
+    val name: String, // Nome que a propriedade terá no XML
+    val type: XMLType = XMLType.ATTRIBUTE, // Tipo de representação no XML (atributo, entidade ou objeto)
+    val ignore: Boolean = false // Indica se a propriedade deve ser ignorada na geração do XML
+)
 
+// Enumeração para os tipos de representação no XML
 enum class XMLType {
-    ATTRIBUTE, ENTITY
+    ATTRIBUTE, // Representação como atributo
+    ENTITY,    // Representação como entidade (elemento filho)
+    OBJECT     // Representação como objeto aninhado
 }
+
 
 // Função para converter um objeto em XML
 fun objectToXML(obj: Any): XMLElement {
@@ -18,8 +72,11 @@ fun objectToXML(obj: Any): XMLElement {
     // Obtendo o nome da classe para criar o elemento XML
     val xmlElement = XMLElement(clazz.simpleName ?: "unknown")
 
-    // Iterando sobre as propriedades da classe
+    // Itera sobre as propriedades da classe do objeto
     clazz.declaredMemberProperties.forEach { prop ->
+        val value = prop.call(obj).toString()
+        val attributeName = prop.name
+
         // Verificando se a propriedade está anotada com XMLProperty
         val annotation = prop.findAnnotation<XMLProperty>()
 
@@ -29,25 +86,37 @@ fun objectToXML(obj: Any): XMLElement {
 
             // Verificando se a propriedade deve ser ignorada
             if (!annotation.ignore) {
-                val value = prop.get(obj as Nothing)
-
-                // Verificando o tipo de tradução (atributo ou entidade)
+                // Verificando o tipo de tradução (atributo, entidade ou objeto)
                 when (annotation.type) {
                     XMLType.ATTRIBUTE -> {
-                        // Adicionando a propriedade como um atributo do elemento XML
-                        xmlElement.addAttribute(propertyName, value.toString())
+                        // Se a propriedade estiver anotada como um atributo XML, adiciona-a como atributo
+                        xmlElement.addAttribute(propertyName, value)
                     }
                     XMLType.ENTITY -> {
                         // Verificando se o valor da propriedade é um objeto
-                        if (value is Any) {
-                            // Convertendo recursivamente o objeto em XML
+                        if (value.isNotEmpty()) {
+                            // Convertendo recursivamente o objeto em XML e adicionando-o como entidade
                             val childElement = objectToXML(value)
-                            // Adicionando o elemento filho ao elemento XML
                             xmlElement.addChild(childElement)
                         }
                     }
+                    XMLType.OBJECT -> {
+                        // Verificando se a propriedade está anotada com @XmlString para aplicar personalização
+                        val stringAdapterAnnotation = prop.findAnnotation<XmlString>()
+                        val adapter = stringAdapterAnnotation?.value?.java?.newInstance()
+                                as? XmlStringAdapter<Any> ?: NoOpXmlStringAdapter()
+
+                        // Aplicando a personalização ao valor da propriedade
+                        val adaptedValue = adapter.adapt(value)
+
+                        // Adicionando a propriedade como um atributo XML
+                        xmlElement.addAttribute(propertyName, adaptedValue)
+                    }
                 }
             }
+        } else {
+            // Se não houver anotação, adiciona a propriedade como atributo com o mesmo nome
+            xmlElement.addAttribute(attributeName, value)
         }
     }
 
@@ -187,6 +256,8 @@ class XMLDocument {
 
     // Método para adicionar atributos globalmente a todos os elementos filhos de um elemento específico
     fun addAttributesGlobal(nameEntity: String, nameAttributeEntity: String, valueAttributeEntity: String) {
+        // Verifica se o documento e o elemento raiz não são nulos
+        rootElement ?: throw IllegalStateException("RootElement is null")
         // Obtendo a árvore de elementos XML a partir do documento
         val xmlTree = rootElement?.findDescendants()
 
@@ -202,6 +273,8 @@ class XMLDocument {
 
     // Método para renomear atributos globalmente em todos os elementos filhos de um elemento específico
     fun renameAttributesGlobal(nameEntity: String, oldName: String, newName: String) {
+        // Verifica se o documento e o elemento raiz não são nulos
+        rootElement ?: throw IllegalStateException("RootElement is null")
         // Obtendo a árvore de elementos XML a partir do documento
         val xmlTree = rootElement?.findDescendants()
 
@@ -217,6 +290,8 @@ class XMLDocument {
 
     // Método para renomear entidades globalmente em todos os elementos filhos de um elemento específico
     fun renameEntitiesGlobal(oldNameEntity: String, newNameEntity: String) {
+        // Verifica se o documento e o elemento raiz não são nulos
+        rootElement ?: throw IllegalStateException("RootElement is null")
         // Obtendo a árvore de elementos XML a partir do documento
         val xmlTree = rootElement?.findDescendants()
 
@@ -231,13 +306,15 @@ class XMLDocument {
     }
 
     // Método auxiliar para obter o índice de um elemento
-    private fun getIndexOfElement(nameAttribute: String, listAttributes: MutableList<XMLAttribute>) : Int {
+    private fun getIndexOfElement(nameAttribute: String, listAttributes: MutableList<XMLAttribute>): Int {
         // Obtendo o índice do elemento com o nome especificado
         return listAttributes.indexOfFirst { it.name == nameAttribute }
     }
 
     // Método para escrever o documento XML em um arquivo
     fun writeToFile(fileName: String) {
+        // Verifica se o documento e o elemento raiz não são nulos
+        rootElement ?: throw IllegalStateException("RootElement is null")
         val xmlString = rootElement?.prettyPrint() ?: ""
         File(fileName).writeText(xmlString)
     }
@@ -254,6 +331,7 @@ interface XMLVisitor {
     fun visit(element: XMLElement)
 }
 
+@Suppress("UNREACHABLE_CODE")
 class XPathEvaluator(private val document: XMLDocument) {
     // Método para avaliar uma expressão XPath e retornar uma lista de elementos correspondentes
     fun evaluate(expression: String): String {
@@ -261,7 +339,6 @@ class XPathEvaluator(private val document: XMLDocument) {
         var xPathEvaluatorString = ""
         var listXPathCompleted = mutableListOf<XMLElement>()
         var listTemp = mutableListOf(document.rootElement!!)
-
 
         fun aux(tagname: String, listXPath: MutableList<XMLElement>): MutableList<XMLElement> {
             val listTempAuxFun = listXPath.toMutableList()
@@ -272,6 +349,7 @@ class XPathEvaluator(private val document: XMLDocument) {
                 }
             }
             return listTempAuxFun
+            return listXPath.filter {it.name == tagname}.flatMap { it.children }.toMutableList()
         }
 
         for (step in path) {
@@ -314,6 +392,7 @@ fun main() {
         root.addAttribute(name, value)
     }
 
+    // Demonstrando visitação de elementos
     document.accept(object : XMLVisitor {
         override fun visit(element: XMLElement) {
             println(element.name)
--- src/test/Tests.kt
+++ src/test/Tests.kt
@@ -1,15 +1,40 @@
-
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.reflect.full.declaredMemberProperties
-import org.junit.jupiter.api.Assertions
 
+class XMLAnnotationsTest {
 
-class XMLTests {
+    data class Person(
+        @XMLProperty(name = "ID")
+        val id: Int,
+        @XMLProperty(name = "Nome")
+        val name: String,
+        @XMLProperty(name = "Idade", type = XMLType.ATTRIBUTE)
+        val age: Int
+    )
+
+    @Test
+    fun testXMLAnnotations() {
+        val person = Person(1, "John", 30)
+        val xmlElement = objectToXML(person)
+
+        // Verificando se as propriedades foram convertidas corretamente com as anotações
+        assertEquals(3, xmlElement.attributes.size)
+
+        // Verificando os nomes e valores dos atributos
+        assertEquals("1", xmlElement.attributes.find { it.name == "ID" }?.value)
+        assertEquals("John", xmlElement.attributes.find { it.name == "Nome" }?.value)
+        assertEquals("30", xmlElement.attributes.find { it.name == "Idade" }?.value)
+
+        // Verificando se a propriedade "age" foi convertida como atributo
+        assertEquals(0, xmlElement.children.size)
+    }
+}
+
+class XMLDocumentTest {
 
     private lateinit var document: XMLDocument
-// Aumentar a arvore de testes
 
     @BeforeEach
     fun setUp() {
@@ -107,7 +132,7 @@ class XMLTests {
     fun `test add attribute globally`() {
         // Teste para garantir que podemos adicionar um atributo a todos os elementos filhos de um elemento específico.
         document.addAttributesGlobal("child", "AnotherTest", "World")
-        assertTrue(document.rootElement?.children?.first()?.hasAttribute("AnotherTest") ?: false)
+        assertEquals("World", document.rootElement?.children?.first()?.attributes?.find { it.name == "AnotherTest" }?.value)
     }
 
     @Test
@@ -116,8 +141,8 @@ class XMLTests {
         val child = document.rootElement?.children?.first()
         child?.addAttribute("OldName", "Value")
         document.renameAttributesGlobal("child", "OldName", "NewName")
-        assertTrue(child?.hasAttribute("NewName") ?: false)
-        assertFalse(child?.hasAttribute("OldName") ?: true)
+        assertEquals("Value", child?.attributes?.find { it.name == "NewName" }?.value)
+        assertNull(child?.attributes?.find { it.name == "OldName" })
     }
 
     @Test
@@ -126,84 +151,6 @@ class XMLTests {
         val child = document.rootElement?.children?.first()
         document.renameEntitiesGlobal("child", "newChild")
         assertEquals("newChild", child?.name)
-    }
-
-    @Test
-    fun `test XPath evaluation`() {
-        // Criando um documento com um elemento raiz "root" e cinco elementos filhos "child1" a "child5"
-        val document = XMLDocument()
-        val root = XMLElement("root")
-        val child = XMLElement("child")
-        document.rootElement = root
-        root.addChild(child)
-        for (i in 1..5) {
-            val secondLayerChild = XMLElement("child$i")
-            child.addChild(secondLayerChild)
-        }
-
-        // Avaliando a expressão XPath "root/child" no documento
-        val evaluator = XPathEvaluator(document)
-        val result = evaluator.evaluate("root/child")
-
-        assertEquals("<child1/>\n<child2/>\n<child3/>\n<child4/>\n<child5/>\n", result)
-
-        // Verificando se o resultado possui exatamente 5 elementos, pois esperamos encontrar os cinco filhos de "root"
-        //assertEquals(5, result.size)
-    }
-}
-
-class XMLAnnotationsTest {
-
-    data class Person(
-        @XMLProperty(name = "ID")
-        val id: Int,
-        @XMLProperty(name = "Nome")
-        val name: String,
-        @XMLProperty(name = "Idade", type = XMLType.ATTRIBUTE)
-        val age: Int
-    )
-
-    @Test
-    fun testXMLAnnotations() {
-        val person = Person(1, "John", 30)
-        val xmlElement = objectToXML(person)
-
-        // Verificando se as propriedades foram convertidas corretamente com as anotações
-        Assertions.assertEquals(3, xmlElement.attributes.size)
-
-        // Verificando os nomes e valores dos atributos
-        Assertions.assertEquals("1", xmlElement.attributes.find { it.name == "ID" }?.value)
-        Assertions.assertEquals("John", xmlElement.attributes.find { it.name == "Nome" }?.value)
-        Assertions.assertEquals("30", xmlElement.attributes.find { it.name == "Idade" }?.value)
-
-        // Verificando se a propriedade "age" foi convertida como atributo
-        Assertions.assertEquals(0, xmlElement.children.size)
-    }
-
-    @Test
-    fun testXMLReflection() {
-        data class TestClass(val field1: String, val field2: Int, val field3: Double)
-
-        val properties = TestClass::class.declaredMemberProperties
-        Assertions.assertEquals(3, properties.size)
-        Assertions.assertEquals("field1", properties[0].name)
-        Assertions.assertEquals("field2", properties[1].name)
-        Assertions.assertEquals("field3", properties[2].name)
-    }
-
-    @Test
-    fun testObjectToXML() {
-        data class TestClass(val field1: String, val field2: Int)
-
-        val obj = TestClass("value1", 42)
-        val xmlElement = objectToXML(obj)
-
-        // Verificando se as propriedades foram convertidas corretamente sem anotações
-        Assertions.assertEquals(2, xmlElement.attributes.size)
-
-        // Verificando os nomes e valores dos atributos
-        Assertions.assertEquals("value1", xmlElement.attributes.find { it.name == "field1" }?.value)
-        Assertions.assertEquals("42", xmlElement.attributes.find { it.name == "field2" }?.value)
     }
 }
 
@@ -236,46 +183,39 @@ class XMLElementTest {
     }
 }
 
-class XMLDocumentTest {
+class XMLTests {
 
     @Test
-    fun testXMLDocument() {
+    fun testXPathEvaluation() {
+        // Criando um documento com um elemento raiz "root" e cinco elementos filhos "child1" a "child5"
         val document = XMLDocument()
         val root = XMLElement("root")
+        val child = XMLElement("child")
         document.rootElement = root
-
-        // Adicionando vários filhos usando um loop
+        root.addChild(child)
         for (i in 1..5) {
-            val child = XMLElement("child$i")
-            root.addChild(child)
+            val secondLayerChild = XMLElement("child$i")
+            child.addChild(secondLayerChild)
         }
 
-        // Renomeando o elemento raiz
-        document.rootElement?.rename("newRoot")
+        // Avaliando a expressão XPath "root/child" no documento
+        val evaluator = XPathEvaluator(document)
+        val result = evaluator.evaluate("root/child")
 
-        // Escrevendo o documento em um arquivo
-        document.writeToFile("test_output.xml")
+        assertEquals("<child1/>\n<child2/>\n<child3/>\n<child4/>\n<child5/>\n", result)
+    }
+}
 
-        // Adicionando vários atributos usando um loop
-        val attributesToAdd = listOf(
-            "attr1" to "value1",
-            "attr2" to "value2",
-            "attr3" to "value3"
-        )
-        for ((name, value) in attributesToAdd) {
-            root.addAttribute(name, value)
-        }
+class XMLReflectionTest {
 
-        // Testando a adição de atributos globalmente
-        document.addAttributesGlobal("newRoot", "globalAttr", "globalValue")
-        assertEquals(4, root.attributes.size)
+    @Test
+    fun testXMLReflection() {
+        data class TestClass(val field1: String, val field2: Int, val field3: Double)
 
-        // Testando a renomeação de atributos globalmente
-        document.renameAttributesGlobal("newRoot", "attr1", "renamedAttr")
-        assertEquals("renamedAttr", root.attributes.find { it.name == "attr1" }?.name)
-
-        // Testando a renomeação de entidades globalmente
-        document.renameEntitiesGlobal("newRoot", "newElement")
-        assertEquals("newElement", root.name)
+        val properties = TestClass::class.declaredMemberProperties
+        assertEquals(3, properties.size)
+        assertEquals("field1", properties.elementAt(0).name)
+        assertEquals("field2", properties.elementAt(1).name)
+        assertEquals("field3", properties.elementAt(2).name)
     }
 }
